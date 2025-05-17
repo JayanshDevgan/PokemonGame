0dd18aead71d143ef6b8881bb2c7b73e8f428309
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2025-05-17 13:36:54 +0530

    Final Words!

--- PokemonGame/main.cpp
+++ PokemonGame/main.cpp
@@ -21,7 +21,14 @@ int main()
 	case 3:
 		std::cout << "You chose Squirtle! A cool choice.\n";
 	default:
-		std::cout << "Invalid Choice\n";
+		std::cout << "Invalid Choice. Please Restart the game.\n";
 	}
+
+	std::cout << "Ah, an excellent choice!" << std::endl;
+	std::cout << "But beware, Trainer," << std::endl;
+	std::cout << "this is only the beginning." << std::endl;
+	std::cout << "Your journey is about to unfold." << std::endl;
+	std::cout << "Now let’s see if you’ve got what it takes to keep going!" << std::endl;
+	std::cout << "Good luck, and remember… Choose wisely!" << std::endl;
 	return 0;
 }
